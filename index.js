23dd3f9d72d805796e2af18cb7e9a73d0bc671cc
James Cahill <jcahill773@gmail.com>
2019-12-14 21:50:18 +0000

    PoC 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fontkit = require("fontkit");
+const express = require("express");
 const fs = require("fs");
 
 function toHex(value) {
@@ -6,7 +7,7 @@ function toHex(value) {
 }
 
 function createSVGTags(path) {
-  return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="${path}" transform="rotate(180) scale(-1,1)" transform-origin="50% 50%"/></svg>`;
+  return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="2048" width="2048"><path d="${path}" transform="rotate(180) scale(-1,1)" transform-origin="50% 50%"/></svg>`;
 }
 
 function glyphToSVGFile(glyph) {
@@ -22,19 +23,65 @@ const chars = font.characterSet;
 
 const data = [];
 
-chars.forEach(point => {
-  let glyph = font.glyphForCodePoint(point);
+chars.forEach(decimalPoint => {
+  const point = toHex(decimalPoint);
+  if (!(["000D", "0020"].indexOf(point) > -1)) {
+    let glyph = font.glyphForCodePoint(decimalPoint);
 
-  data.push({
-    point: toHex(point),
-    file: glyphToSVGFile(glyph)
+    data.push({
+      point: point,
+      file: glyphToSVGFile(glyph)
+    });
+  } else {
+    console.log(`skipping default character ${point}`);
+  }
+});
+
+console.log(`${font.numGlyphs} glyphs available.`);
+console.log(`Loaded ${data.length} glyphs.`);
+
+//data.forEach(i => {
+//  fs.writeFileSync(`out/${i.point}.svg`, i.file);
+//});
+
+const app = express();
+
+//app.use("/static", express.static("web"));
+app.get("/webview", (req, res) => {
+  res.render("index.ejs", {
+    points: data,
+    fontFullName: font.fullName
   });
 });
 
-console.log(`Loaded ${data.length} glyphs.`);
-
-data.forEach(i => {
-  fs.writeFileSync(`out/${i.point}.svg`, i.file);
+app.get("/info", (req, res) => {
+  res.json({
+    postscriptName: font.postscriptName,
+    fullName: font.fullName,
+    familyName: font.familyName,
+    subFamilyName: font.subFamilyName,
+    copyright: font.copyright,
+    version: font.version
+  });
 });
 
-console.log("Saved!");
+app.get("/points", (req, res) => {
+  let out = [];
+  data.forEach(i => {
+    out.push(i.point);
+  });
+  res.json(out);
+});
+
+app.get("/points/:p", (req, res) => {
+  const found = data.find(i => i.point == req.params.p);
+  if (found != undefined) {
+    res.type("image/svg+xml").send(found.file);
+  } else {
+    res.status(404).json({ error: "glyph not found" });
+  }
+});
+
+app.listen(3000);
+
+console.log("Ready!");
