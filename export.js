2a87597fac4fd3300aab75191bf68cc2a0e908f1
James Cahill <jcahill773@gmail.com>
2019-12-15 10:26:40 +0000

    added node 6 requirement (from fontkit) & added export feature

---
+++ export.js
@@ -0,0 +1,39 @@
+const empty = require("empty-dir");
+const fs = require("fs");
+
+// Console formatting
+const console = require("prefix-logger")("fontx.export");
+require("colors");
+
+module.exports = (obj, outputFolderName) => {
+  // Create the output folder if it does not already exist.
+  try {
+    if (!fs.existsSync(outputFolderName)) {
+      fs.mkdirSync(outputFolderName);
+      console.log(`created folder '${outputFolderName}'`.grey);
+    }
+  } catch (err) {
+    console.error(`Error creating folder '${outputFolderName}': ${err}`);
+    process.exit(1);
+  }
+
+  // Check if the directory is empty.
+  if (!empty.sync(outputFolderName)) {
+    console.error(`Folder '${outputFolderName}' not empty!`);
+    process.exit(2);
+  }
+
+  console.log("saving glyphs...".grey);
+
+  // Save the files.
+  try {
+    obj.data.forEach(i => {
+      fs.writeFileSync(`${outputFolderName}/${i.point}.svg`, i.file);
+    });
+  } catch (err) {
+    console.error(`Error saving files: ${err}`);
+    process.exit(3);
+  }
+
+  console.log("done!".green);
+};
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const core = require("./core");
 const express = require("./express");
+const exp = require("./export");
 
+// This is required
 const Core = core("font.ttf");
-const Express = express(Core, 3000);
+
+// You may now use optional features
+
+// Express webserver
+// const Express = express(Core, 3000);
+
+// Export all glyphs to a directory
+// const Export = exp(Core, "out");
